bec404227dc721012ed6a17845cbc319b61fc9b7
Khoo Ven Jin <venjin_boy@hotmail.com>
2024-08-01 19:08:49 +0800

    feat: add index declares

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue"
+import "./style.css"
+import router from "./router"
+import { pinia } from "./utils/pinia-state-management"
+import App from "./views/Home.vue"
+
+createApp(App).use(pinia).use(router).mount("#app")
