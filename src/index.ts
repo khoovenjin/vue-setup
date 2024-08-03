7d025212013cf49550241f2e891874fcbb385623
Khoo Ven Jin <venjin_boy@hotmail.com>
2024-08-03 15:57:05 +0800

    fix: update index page

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue"
+import App from "./App.vue"
 import router from "./router"
 import { pinia } from "./utils/pinia-state-management"
-import App from "./views/Home.vue"
 
 createApp(App).use(pinia).use(router).mount("#app")
