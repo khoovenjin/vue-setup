2f50370cdd2427a0d8baf057862419e1a72d5ccd
Khoo Ven Jin <venjin_boy@hotmail.com>
2024-09-06 12:17:10 +0800

    feat: append vant base css

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import { pinia } from "./utils/pinia-state-management"
+import "vant/lib/style/base.css"
 
 createApp(App).use(pinia).use(router).mount("#app")
